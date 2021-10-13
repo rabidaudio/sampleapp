f570efcbf42c71924cb198d8b37fd5fed9b79332
Charles Julian Knight <julian@fixdapp.com>
2021-10-13 12:51:06 -0400

    add api method

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 
+const ytdl = require('ytdl-core')
+const ffmpeg = require('fluent-ffmpeg')
+
 const app = express()
 
 app.set('view engine', 'ejs')
@@ -9,6 +12,20 @@ app.get('/', (req, res) => {
   res.render('index', {
     name: req.query.name || 'world'
   })
+})
+
+// expects query ?v= of youtube video id
+app.get('/api/audio.mp3', (req, res) => {
+  res.set({ 'Content-Type': 'audio/mpeg' })
+  ffmpeg(ytdl(`http://www.youtube.com/watch?v=${req.query.v}`, {
+    filter: 'audio'
+  }))
+    .audioCodec('libmp3lame')
+    .noVideo()
+    .outputFormat('mp3')
+    .on('error', (err) => console.error(err))
+    .on('end', () => console.log('Audio stream finised'))
+    .pipe(res, { end: true })
 })
 
 const port = process.env.PORT || 5000
